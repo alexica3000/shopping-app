6aacae8c6acfe3e01928b4001f7f27741767d297
Alex <sanatateacom@gmail.com>
2020-09-30 15:29:08 +0300

    added few recipes on list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a simply test', 'https://cdn.pixabay.com/photo/2015/10/26/07/21/soup-1006694_960_720.jpg')
+    new Recipe('A test Recipe', 'This is a simply test', 'https://cdn.pixabay.com/photo/2015/10/26/07/21/soup-1006694_960_720.jpg'),
+    new Recipe('Recipe 2', 'This is a simply description', 'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg'),
+    new Recipe('A recipe 3', 'Simply desc', 'https://cdn.pixabay.com/photo/2015/09/17/17/19/egg-944495_960_720.jpg'),
   ];
 
   constructor() { }
