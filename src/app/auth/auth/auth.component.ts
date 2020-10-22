1414c32320ea2376dd3f55e37a6b804c06a934f7
alex <sanatateacom@gmail.com>
2020-10-22 16:17:40 +0300

    created auth form

---
+++ src/app/auth/auth/auth.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  public isLoginMode = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSwitchMode(): void {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form.value);
+    form.reset();
+  }
+}
